7b448e1ad432dd7e66d6d26aad9db1d6a5405228
xinn15 <114967620+xinn15@users.noreply.github.com>
2023-04-12 11:39:52 +0800

    Add files via upload

--- TheMuseHotel/instruction2.js
+++ TheMuseHotel/instruction2.js
@@ -8,7 +8,7 @@ class instruction2 extends Phaser.Scene {
 
     preload(){
     // intro image
-    this.load.image('instruction2', 'assets/instruction2.jpg');
+    this.load.image('instruction2', 'assets/Instruction2.jpg');
     
     }
 
@@ -16,7 +16,7 @@ class instruction2 extends Phaser.Scene {
         this.add.image(0, 0, 'instruction2').setOrigin(0, 0);
         this.spacesnd = this.sound.add("space").setVolume(0.2);
 
-        console.log("This is instruction3");
+        console.log("This is instruction2");
 
         //this.input.once('pointerdown', function(){
         var spaceDown = this.input.keyboard.addKey('SPACE');
